541cbe1c3dbf7629beb15dc8bff7a49201136f57
Cesar de Carvalho <cesarncarvalho@gmail.com>
2020-02-13 18:35:13 -0300

    [LEMONEY-11]
    - Configuring the cross origin permissions for external clients.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,11 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://lemoney-admin.herokuapp.com'
+      resource '*'
+    end
+  end
 end
